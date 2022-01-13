230a93694cc4589171919c9e42c347795d646060
hekang42 <hekang@student.42seoul.kr>
2022-01-13 15:56:43 +0900

    git Init Project

---
+++ Git-Challenges/Git-Challenges/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Git-Challenges
+//
+//  Created by 강희영 on 2022/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Git-Challenges/Git-Challenges/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git-Challenges
+//
+//  Created by 강희영 on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
