0403435c0adc04671f603a081f1bd64fc781c4f1
hekang42 <hekang@student.42seoul.kr>
2022-01-17 14:09:27 +0900

    [feat] Create Contribution View & add Swift Soup with Spm

--- Git-Challenges/Git-Challenges/View/ContentView.swift
+++ Git-Challenges/Git-Challenges/View/ContentView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NameCardView()
+        ContributionView()
     }
 }
 
