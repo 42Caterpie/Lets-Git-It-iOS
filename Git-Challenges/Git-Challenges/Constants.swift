01ee45bfd84d55039c04800fa28a7640be0ffdb0
Eunbin Kwon <kwoneunbini@gmail.com>
2022-01-17 15:44:56 +0900

    [feat] Modify Color Theme Constants

--- Git-Challenges/Git-Challenges/Constants.swift
+++ Git-Challenges/Git-Challenges/Constants.swift
@@ -6,28 +6,57 @@
 //
 
 import Foundation
+import SwiftUI
+
+// Usage:
+// UserDefault에 theme을 key로 하는 객체 저장
+// UserDefault.standard.set(ColorPalette.pink, forKey: "theme")
+// let theme = UserDefault.standard.object("theme")
 
 struct ColorPalette {
-    enum Pink {
-        static var progressDone = 0xE68181
-        static var pinkLevelOne = 0xF0C1C1
-        static var pinkLevelTwo = 0xFBAAAA
-        static var pinkLevelThree = 0xE78181
-    }
     
-    enum Blue {
-        //
-    }
+    static var pink: ([String: Color], [String: String]) = (
+        [
+            "progressBar": Color(hex: 0xE68181),
+            "levelOne": Color(hex: 0xF0C1C1),
+            "levelTwo": Color(hex: 0xFBAAAA),
+            "levelThree": Color(hex: 0xE78181),
+            "defaultGray": Color(hex: 0xC4C4C4)
+        ],
+        [
+            "notComitted": "😡",
+            "committed": "🔥"
+            
+        ]
+    )
     
-    enum Green {
-        //
-    }
+    static var blue: ([String: Color], [String: String]) = (
+        [
+            "progressBar": Color(hex: 0x6DA8DB),
+            "levelOne": Color(hex: 0xC1DAF0),
+            "levelTwo": Color(hex: 0xA0C7EA),
+            "levelThree": Color(hex: 0x6DA8DB),
+            "defaultGray": Color(hex: 0xC4C4C4)
+        ],
+        [
+            "notComitted": "❄️",
+            "committed": "🌊"
+            
+        ]
+    )
     
-    static var defaultGray = 0xC4C4C4
-}
-
-struct Emoji {
-    enum Pink {
-        static var progressIndicatior: String = "🔥"
-    }
+    static var green: ([String: Color], [String: String]) = (
+        [
+            "progressBar": Color(hex: 0x85C767),
+            "levelOne": Color(hex: 0xCAEBBB),
+            "levelTwo": Color(hex: 0xAFDB9B),
+            "levelThree": Color(hex: 0x85C767),
+            "defaultGray": Color(hex: 0xC4C4C4)
+        ],
+        [
+            "notComitted": "🍃",
+            "committed": "🌿"
+            
+        ]
+    )
 }
