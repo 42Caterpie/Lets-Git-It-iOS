fb1ffdad577175cbee8dd9a8c374435d96f8f20d
hekang42 <hekang@student.42seoul.kr>
2022-01-17 23:02:40 +0900

    [FEAT] modify constants functions

--- Git-Challenges/Git-Challenges/Constants.swift
+++ Git-Challenges/Git-Challenges/Constants.swift
@@ -13,50 +13,85 @@ import SwiftUI
 // UserDefault.standard.set(ColorPalette.pink, forKey: "theme")
 // let theme = UserDefault.standard.object("theme")
 
+enum color: Int {
+    case defaultGray = 0
+    case levelOne
+    case levelTwo
+    case levelThree
+    case progressBar
+}
+
+enum emoji: Int {
+    case notCommitted = 0
+    case committed
+}
+
 struct ColorPalette {
+    static var pink: ([Color], [String]) = (
+    [
+        Color(hex: 0xC4C4C4),
+        Color(hex: 0xF0C1C1),
+        Color(hex: 0xFBAAAA),
+        Color(hex: 0xE78181),
+        Color(hex: 0xE68181)
+    ],
+    [
+        "😡",
+        "🔥"
+    ]
+)
     
-    static var pink: ([String: Color], [String: String]) = (
+    static var blue: ([Color], [String]) = (
         [
-            "progressBar": Color(hex: 0xE68181),
-            "levelOne": Color(hex: 0xF0C1C1),
-            "levelTwo": Color(hex: 0xFBAAAA),
-            "levelThree": Color(hex: 0xE78181),
-            "defaultGray": Color(hex: 0xC4C4C4)
+            Color(hex: 0xC4C4C4),
+            Color(hex: 0xC1DAF0),
+            Color(hex: 0xA0C7EA),
+            Color(hex: 0x6DA8DB),
+            Color(hex: 0x6DA8DB),
         ],
         [
-            "notComitted": "😡",
-            "committed": "🔥"
-            
+            "❄️",
+            "🌊"
         ]
     )
     
-    static var blue: ([String: Color], [String: String]) = (
+    static var green: ([Color], [String]) = (
         [
-            "progressBar": Color(hex: 0x6DA8DB),
-            "levelOne": Color(hex: 0xC1DAF0),
-            "levelTwo": Color(hex: 0xA0C7EA),
-            "levelThree": Color(hex: 0x6DA8DB),
-            "defaultGray": Color(hex: 0xC4C4C4)
+            Color(hex: 0xC4C4C4),
+            Color(hex: 0xCAEBBB),
+            Color(hex: 0xAFDB9B),
+            Color(hex: 0x85C767),
+            Color(hex: 0x85C767),
         ],
         [
-            "notComitted": "❄️",
-            "committed": "🌊"
-            
+            "🍃",
+            "🌿"
         ]
     )
     
-    static var green: ([String: Color], [String: String]) = (
-        [
-            "progressBar": Color(hex: 0x85C767),
-            "levelOne": Color(hex: 0xCAEBBB),
-            "levelTwo": Color(hex: 0xAFDB9B),
-            "levelThree": Color(hex: 0x85C767),
-            "defaultGray": Color(hex: 0xC4C4C4)
-        ],
-        [
-            "notComitted": "🍃",
-            "committed": "🌿"
-            
-        ]
-    )
+    func getColors(_ color: String) -> [Color] {
+        switch color {
+        case "pink":
+            return ColorPalette.pink.0
+        case "blue":
+            return ColorPalette.blue.0
+        case "green":
+            return ColorPalette.green.0
+        default:
+            return []
+        }
+    }
+    
+    func getEmoji(_ color: String) -> [String] {
+        switch color {
+        case "pink":
+            return ColorPalette.pink.1
+        case "blue":
+            return ColorPalette.blue.1
+        case "green":
+            return ColorPalette.green.1
+        default:
+            return []
+        }
+    }
 }
