ab8854d2e321820324cb56eaa14b787565ce3fde
hekang42 <hekang@student.42seoul.kr>
2022-01-24 16:03:50 +0900

    [Feat] change init color : pink -> green, modify refresh button #35

--- Git-Challenges/Let's Git it!/Util/Theme.swift
+++ Git-Challenges/Let's Git it!/Util/Theme.swift
@@ -10,14 +10,14 @@ import SwiftUI
 
 func getThemeColors() -> [Color] {
     let colorPalette = ColorPalette()
-    let themeColor: String = UserDefaults.standard.string(forKey: "ColorTheme") ?? "pink"
+    let themeColor: String = UserDefaults.standard.string(forKey: "ColorTheme") ?? "green"
     UserDefaults.standard.setValue(themeColor, forKey: "ColorTheme")
     return colorPalette.getColors(themeColor)
 }
 
 func getThemeEmojis() -> [String] {
     let colorPalette = ColorPalette()
-    let themeColor: String = UserDefaults.standard.string(forKey: "ColorTheme") ?? "pink"
+    let themeColor: String = UserDefaults.standard.string(forKey: "ColorTheme") ?? "green"
     UserDefaults.standard.setValue(themeColor, forKey: "ColorTheme")
     return colorPalette.getEmoji(themeColor)
 }
