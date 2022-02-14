6c691dc83e7f42b5510b4d92928e82878978264f
hekang42 <hekang@student.42seoul.kr>
2022-02-14 22:13:20 +0900

    [feat] init Competition View

---
+++ Git-Challenges/Let's Git it!/Model/roomData.swift
@@ -0,0 +1,13 @@
+//
+//  RoomData.swift
+//  Let's Git it!
+//
+//  Created by 강희영 on 2022/02/14.
+//
+
+struct RoomData {
+    var id: Int
+    var title: String
+    var startDate: String
+    var goal: Int
+}
