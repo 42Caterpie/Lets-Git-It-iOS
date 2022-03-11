3e058500ca426e15052b37b143b022022a8161c4
hekang42 <hekang@student.42seoul.kr>
2022-03-11 14:38:42 +0900

    [feat] modify Competition List View with refactoring

--- Git-Challenges/Let's Git it!/View/CompetitionMainView.swift
+++ Git-Challenges/Let's Git it!/View/CompetitionMainView.swift
@@ -8,133 +8,129 @@
 import SwiftUI
 
 struct CompetitionMainView: View {
+    @State private var backgroundBlur: CGFloat = 0
+    @State private var halfModalOffset = uiSize.height - 500
+    @State private var showActionSheet: Bool = false
     @State private var showCreateRoomModal: Bool = false
     @State private var showJoinRoomModal: Bool = false
-    @ObservedObject var competitionService = CompetitionService()
-    @State private var backgroundBlur: CGFloat = 0
-    @State var sheetMode: SheetMode = .half
     @ObservedObject var keyboard: KeyboardObserver = KeyboardObserver()
-    @State private var quaterModalOffset = uiSize.height / 2
+    @ObservedObject var competitionService = CompetitionService()
+    
+    private func toolbar() -> some View {
+        HStack {
+            Spacer()
+            Button  {
+                if (!(showActionSheet || showJoinRoomModal || showCreateRoomModal)) {
+                    showActionSheet = true
+                }
+            } label: {
+                Image(systemName: "plus")
+                    .resizable()
+                    .frame(width: 23, height: 23)
+                    .foregroundColor(.black)
+                    .padding()
+            }
+        }
+    }
+    
+    private func halfModalView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .foregroundColor(.white)
+                .shadow(radius: 10)
+            VStack {
+                if showJoinRoomModal {
+                    JoinRoomModalView()
+                        .environmentObject(competitionService)
+                        .frame(height: 350)
+                } else {
+                    CreateRoomModalView()
+                        .environmentObject(competitionService)
+                        .frame(height: 350)
+                }
+                Spacer()
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .offset(y: halfModalOffset - (keyboard.isShowing ? keyboard.height - 80 : 0))
+        .animation(.spring())
+    }
+    
+    private func competitionRoomCell(_ room: RoomData) -> some View {
+        return HStack {
+            VStack (alignment: .leading) {
+                Text("\(room.title)")
+                    .font(.system(size: 18, weight: .bold))
+                Text("\(room.startDate) ~")
+            }
+            Spacer()
+        }
+        .modifier(CardModifier(height: 144))
+        .padding([.vertical], 5)
+    }
+    
+    private func gameListandModalView() -> some View {
+        ZStack {
+            ScrollView {
+                HStack {
+                    Text("Game List")
+                        .bold()
+                        .padding(.horizontal)
+                    Spacer()
+                }
+                VStack {
+                    ForEach (competitionService.roomDatas, id: \.self.id) { room in
+                        NavigationLink {
+                            CompetitionRoomView(of: room.id)
+                                .environmentObject(competitionService)
+                        } label: {
+                            competitionRoomCell(room)
+                        }
+                    }
+                    .buttonStyle(.plain)
+                }
+                Spacer()
+            }
+            .disabled(showActionSheet || showJoinRoomModal || showCreateRoomModal)
+            .onTapGesture {
+                showActionSheet = false
+                showJoinRoomModal = false
+                showCreateRoomModal = false
+            }
+            .blur(radius: showJoinRoomModal || showActionSheet || showCreateRoomModal ? 3 : 0)
+            if showJoinRoomModal || showCreateRoomModal {
+                halfModalView()
+            }
+        }
+    }
+    
+    private func makeJoinActionsheet() -> ActionSheet {
+        return ActionSheet(
+            title: Text("Select Make Room or Join Room"),
+            buttons: [
+                .default(
+                    Text("Make Room"),
+                    action: { showCreateRoomModal = true }),
+                .default(Text("Join Room"), action: { showJoinRoomModal = true }),
+                .cancel({ showActionSheet = false })
+            ])
+    }
     
     var body: some View {
-        NavigationView {
-            VStack {
-                HStack () {
-                    Spacer()
-                    Button  {
-                        showJoinRoomModal = true
-                    } label: {
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: 23, height: 23)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                }
-                ZStack {
-                    ScrollView {
-                        
-                        HStack {
-                            Text("Game List")
-                                .bold()
-                                .padding(.horizontal)
-                            Spacer()
-                        }
-                        
-                        VStack {
-                            ForEach (competitionService.roomDatas, id: \.self.id) { room in
-                                NavigationLink {
-                                    CompetitionRoomView(of: room.id)
-                                        .environmentObject(competitionService)
-                                } label: {
-                                    HStack {
-                                        VStack (alignment: .leading) {
-                                            Text("\(room.title)")
-                                                .bold()
-                                            Text("\(room.startDate) ~")
-                                            //                                        Text("Goal: \(room.goal)")
-                                        }
-                                        Spacer()
-                                    }
-                                    .modifier(CardModifier(height: 144))
-                                    .padding([.vertical], 5)
-                                }
-                            }
-                            .buttonStyle(.plain)
-                            //
-                            //                        VStack {
-                            //                            Image(systemName: "plus.circle")
-                            //                                .resizable()
-                            //                                .frame(width: 50, height: 50)
-                            //                            Text("방 만들기")
-                            //                        }
-                            //                        .onTapGesture {
-                            //                            showCreateRoomModal = true
-                            //                        }
-                            //                        .modifier(CardModifier(height: 140))
-                            //                        .background(
-                            //                            RoundedRectangle(cornerRadius: 20)
-                            //                                .foregroundColor(ColorPalette.green.0[1])
-                            //                                .opacity(0.8)
-                            //                        )
-                            //                        .padding([.vertical], 5)
-                        }
-                        Spacer()
-                    }
-                    
-                    .disabled(showJoinRoomModal || showCreateRoomModal)
-                    .blur(radius: showJoinRoomModal ? 3 : 0)
-                    
-                    if showJoinRoomModal {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 30)
-                                .foregroundColor(.white)
-                                .shadow(radius: 10)
-                            VStack {
-                                HStack {
-                                    Spacer()
-                                    Button {
-                                        showJoinRoomModal = false
-                                    } label: {
-                                        Text("Cancle")
-                                    }
-                                    .padding()
-                                }
-                                JoinRoomModalView()
-                                    .environmentObject(competitionService)
-                                    .frame(height: 300)
-                                Spacer()
-                            }
-                        }
-                        .animation(.spring())
-                        .offset(y: quaterModalOffset - (keyboard.isShowing ? keyboard.height : 0))
-                        
-                    }
-                }
-                //                .sheet(isPresented: self.$showCreateRoomModal) {
-                //                    CreateRoomModalView()
-                //                        .environmentObject(competitionService)
-                //                }
-                //                .sheet(isPresented: self.$showJoinRoomModal) {
-                //
-                //                }
-                
-                //                .customBottomSheet(isPresented: self.$showJoinRoomModal) {
-                //                    JoinRoomModalView()
-                //                        .environmentObject(competitionService)
-                //                        .frame(height: 400)
-                ////                        .clearModalBackground()
-                //                }
-                
-                
+        VStack {
+            toolbar()
+            gameListandModalView()
+            .actionSheet(isPresented: $showActionSheet) {
+                makeJoinActionsheet()
             }
-            .onAppear {
-                self.keyboard.addObserver()
-            }
-            .onDisappear {
-                self.keyboard.removeObserver()
-            }
-            .modifier(NavigationBarModifier())
+        }
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
+        .onAppear {
+            self.keyboard.addObserver()
+        }
+        .onDisappear {
+            self.keyboard.removeObserver()
         }
     }
 }
